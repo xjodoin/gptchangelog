f9251d4f8d29292700f79d194ede75b770928f82
Xavier Jodoin <xavier.jodoin@greybox.biz>
2023-04-13 12:26:17 -0400

    Fix deploy

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="gptchangelog",
-    version="0.1.0",
+    version="0.1.1",
     author="Xavier Jodoin",
     author_email="xavier@jodoin.me",
     description="Automatically generate a changelog using GPT-3",
