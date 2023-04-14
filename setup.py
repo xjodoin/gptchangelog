f260f9977558de6a307e3882bf6a3f8d8780381a
Xavier Jodoin <xavier.jodoin@greybox.biz>
2023-04-14 14:39:55 -0400

    chore: Bump version to 0.2.0 and update changelog formatting.
    - Update version number and install requirements in `setup.py`
    - Refactor and update `CHANGELOG.md`, including fixes for previous version
    - Prepare for new release with version `0.2.0`

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="gptchangelog",
-    version="0.1.1",
+    version="0.2.0",
     author="Xavier Jodoin",
     author_email="xavier@jodoin.me",
     description="Automatically generate a changelog using GPT-3",
